1a4d95bfbfc5a0abe09ac53925cf41f0f3a4cd33
Dulmi Wijenayaka <97942401+Dulmi-2000@users.noreply.github.com>
2023-09-04 23:49:11 +0530

    validation of user inputs of payment selection

--- lib/Pages/payment_selection.dart
+++ lib/Pages/payment_selection.dart
@@ -2,7 +2,7 @@ import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
-
+import 'package:npaly_application/Pages/card_paymnet.dart';
 
 class PaymentFirstPage extends StatefulWidget{
   const PaymentFirstPage({super.key});
@@ -13,7 +13,7 @@ class PaymentFirstPage extends StatefulWidget{
 
 class _PaymentFirstPageState extends State<PaymentFirstPage> {
 
-           //text editing controller for textbox
+    //text editing controller for textbox
   //final myController = TextEditingController();
   TextEditingController myController = TextEditingController();
 
@@ -27,12 +27,49 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
   var items=["Gym Fees","Club Fees","Pool"];
   
 
+
+//error messages
+String dropdownError = '';
+  String amountError = '';
+
+
+
+
   @override
   Widget build(BuildContext context){
 
     Size size=MediaQuery.of(context).size;
 
     return Scaffold(
+
+
+
+           appBar: AppBar(
+
+
+          title: const Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Text("Payment",
+            style: TextStyle(
+              fontSize: 22,
+              letterSpacing: 0.3,
+            ),
+            ),
+          ),
+          backgroundColor: HexColor('#39B54A'),
+
+
+
+          //to chnge app bar size
+
+           bottom:  const PreferredSize( 
+            preferredSize: Size.fromHeight(0.01), 
+            child: Padding(padding: EdgeInsets.only(left: 15),
+            ),
+            ),
+        ),
+
+
     
       //background color of the page
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
@@ -57,7 +94,7 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
     
                //space in top
                 SizedBox(
-                  height: size.height/15,
+                  height: size.height/45,
                 ),
     
     
@@ -66,17 +103,16 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
                 //text- pay here
                 const Text("Pay Here",
                 style: TextStyle(
-                  fontSize: 32,
+                  fontSize: 25,
                   fontWeight: FontWeight.w900,
                 ),
                 ),
     
     
-    
-    
+        
                //space between 2 texts
                 SizedBox(
-                  height: size.height/40,    
+                  height: size.height/80,    
                 ),
     
     
@@ -89,12 +125,19 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
     
                ),    
                ),
-    
+
+
+               
+               //space between text and image
+            /*    SizedBox(
+                  height: size.height/500,    
+                ),
+    */
               
     
                //image
                 Image.asset("pay.png",
-                height: size.height/2.078,
+                height: size.height/2.2,
                 width: size.width/1.2,
                 ),
     
@@ -103,42 +146,48 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
     
               ),
               ),
+
+                  
+              //space between image and dropdown list
+             SizedBox(
+              height: size.height/5200,
+              width: size.width/3,
+              ),
+
     
     
                Expanded(
                 flex: 1,
                 child: Column(
                   children: [
+            
     
+    
+             //constrained box for dropdown list
 
 
-
-    
-              //space between image and dropdown list
-             SizedBox(
-              height: size.height/250,
-              width: size.width/3,
-              ),
-    
-    
-    
-        
-    
-    
-             //sized box for dropdown list
-            SizedBox(
-
-
-
-              //height and width of the list
-               height: size.height/20,
-               width: size.width/1.2,
-              
-    
-    
+             //this will avoid that the change of the textfeild 
+             //when error text is displaying
+            ConstrainedBox(
     
     
               //dropdown button
+              constraints: BoxConstraints(
+               //height and width of the list
+
+               //minimum height of the textfeild
+               minHeight: size.height/20,
+               //maximum width of the text feild
+               maxWidth: size.width/1.2,
+
+
+
+
+
+              ),
+
+
+
               child: DropdownButtonFormField2<String>(
                 
     
@@ -160,10 +209,17 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
                     ),
                    
                   ),
-                  // Add more decorations..
+                               
+
+                               
+                        //error messsage
+                         errorText: dropdownError.isNotEmpty ? dropdownError : null,
+    
+
+
                 ),
-    
-    
+                        
+
     
     
                   //text to display in the top of the list - fee type text
@@ -197,7 +253,16 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
                     .toList(),
                
                   onChanged: (value) {
+
+
                   //Do something when selected item is changed.
+                        setState(() {
+                          dropdownvalue = value!;
+                          dropdownError = ''; // Clear the error when a selection is made
+                        
+                        }
+                        );
+
                 },
                   onSaved: (value) {
                   value = value.toString();
@@ -262,24 +327,31 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
     
     
                 //amount of the payment
-                            SizedBox(
-                      
-                              height: size.height/20,
-                              width: size.width/1.2,
-                     
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minHeight: size.height/20,
+                                maxWidth: size.width/1.2
+                              ),
+
+
                               child: TextField(
-                      
+                                                  
                                 controller: myController,
-    
+                            
+                            
+                                
                                  keyboardType: TextInputType.number,
                               
                                 decoration: InputDecoration(
-
+                            
                                   contentPadding: const EdgeInsets.symmetric(vertical: 8,horizontal: 12),
-             
-                      
+                                         
+                                                  
                                   //display text
                                         hintText:"Amount (LKR)",
+                            
+                            
+                            
                                         hintStyle: TextStyle(
                                          color: HexColor('#39B54A')
                                         ),
@@ -292,27 +364,48 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
                                           )
                                         ),
                                         
+                                         
+                            
+                            
+                            
+                            
+                                         //error message
+                            
+                                         
+                                    errorText: amountError.isNotEmpty ? amountError : null,
+                                    errorMaxLines: 1,
+                                    errorStyle: const TextStyle(
+                                    color: Colors.red,
+                                    fontSize: 13,
+                                    
+                            
+                            
+                            
+                                   
+                                  ),
+                            
+                            
+                            
                                      ),
                                
                                 style: TextStyle(
-                      
+                                                  
                                 fontSize: 16,
                                  color: HexColor('#39B54A'),
                                 //color: Color.fromARGB(235, 158, 157, 154),
                                 letterSpacing: 0.3,
+                                                 
                                 
-                               
-                                
-                              ),),
-                             
+                              ),
+                                                         ),
                             ),
                           
                              
     
     
-                         //space between amount box cancel button 
+                         //space between amount box continue button 
                         SizedBox(
-                          height: size.height/20
+                          height: size.height/25
                           ),
     
     
@@ -320,18 +413,28 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
 
                    //continue button
 
-                             SizedBox(
-                               height: size.height/20,
-                              width: size.width/1.2,
+                    SizedBox(
+                      height: size.height/20,
+                      width: size.width/1.2,
                              
                       
                              
-                   child: ElevatedButton(
-                   style: ElevatedButton.styleFrom(
-                   backgroundColor: HexColor("#39B54A"),
-                  ),
+                       child: ElevatedButton(
+                       style: ElevatedButton.styleFrom(
+                       backgroundColor: HexColor("#39B54A"),
+                     ),
+
                     onPressed:() {
-                      
+
+
+                   // doValidation(BuildContext);
+
+                    
+                  
+                   doValidation();
+                        
+                   
+
                     },
                       
                       
@@ -357,5 +460,109 @@ class _PaymentFirstPageState extends State<PaymentFirstPage> {
 
       );
 
+
+
+      
+
+  }
+
+//validation of user inputs
+
+void doValidation() {
+    String userAmount = myController.text.trim();
+    if (dropdownvalue != "Fee Type" && userAmount.isNotEmpty) {
+      try {
+
+
+
+        
+        int parsedAmount = int.parse(userAmount);
+
+
+             if(parsedAmount<100)
+          {
+             setState(() {
+            amountError = 'Minimum amount for $dropdownvalue is Rs.100';
+          });
+          }
+
+
+        else if(parsedAmount > 100) {
+          // Both dropdown and text field are valid
+          
+          
+          // Navigate to the CardPaymentPage
+         
+            Navigator.push(context,
+           MaterialPageRoute(builder: (context) =>  const CardPaymentPage(inputText: '',),
+          
+
+
+            ),
+          );
+
+     
+
+
+        } else {
+          // Display an error text for invalid input
+          setState(() {
+            amountError = 'Please enter a valid positive integer for Amount.';
+          });
+        }
+
+
+      } catch (e) {
+        // Handle invalid input (e.g., non-numeric input)
+        setState(() {
+          amountError = 'Please enter a valid integer for Amount.';
+        });
+      }
+
+
+
+    } else {
+      // Display error text for missing input
+      setState(() {
+
+
+        if (dropdownvalue == "Fee Type") {
+          dropdownError = 'Please select a Fee Type.';
+        }
+
+
+        if (userAmount.isEmpty) {
+          amountError = 'Please enter an Amount.';
+        }
+
+      
+
+
+      }
+      );
+    }
   }
 }
+
+
+
+/*
+  void doValidation(BuildContext)
+  {
+    userAmount=myController.text;
+    userAmount=int.parse(amount as String) as String;
+
+    if(userAmount.isNotEmpty)
+    {
+         Navigator.push(context,
+         MaterialPageRoute(builder: (context) =>  const CardPaymentPage()),);
+
+    }
+
+    else{
+     
+    }
+  }
+
+  */
+ 
